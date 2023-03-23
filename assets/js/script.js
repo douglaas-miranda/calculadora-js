c10d5d0d8815e614003da31b52f9c92d6e6a9ed3
Douglas Miranda <douglas.miranda250296@gmail.com>
2023-03-22 22:11:53 -0300

    :white_check_mark: Adicionando funcionalidade de cálculos em js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
 function inserir(num) {
-  document.querySelector('.tela').innerHTML = num;  
+  document.querySelector('.tela').innerHTML += num;  
+}
+
+function limpar() {
+  document.querySelector('.tela').innerHTML = "";
+}
+
+function apagar() {
+  let tela = document.querySelector('.tela').innerHTML;
+  document.querySelector('.tela').innerHTML = tela.substring(0, tela.length-1);
+}
+
+function somar () {
+  let tela = document.querySelector('.tela').innerHTML;  
+  if(tela) {
+    document.querySelector('.tela').innerHTML = eval(tela);
+  }else {
+    document.querySelector('.tela').innerHTML = "nenhum comando"
+  }
 }
